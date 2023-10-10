91ce4a91f86e5c00fa7c909eb27cbab6bf316cdb
Rahul Singh Parmar <rkparmar41@gmail.com>
2023-10-10 12:31:24 +0530

    update ignored files

--- .next/static/chunks/app/(front)/company/page-b3e3313ca4886bac.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[691],{3026:function(e,t,r){Promise.resolve().then(r.bind(r,6404))},6404:function(e,t,r){"use strict";r.r(t);var n=r(7437);t.default=()=>(0,n.jsxs)("a",{href:"",onClick:e=>{e.preventDefault()},className:" ml-auto text-red-600",children:[(0,n.jsx)("span",{className:" text-red-700  text-2xl",children:"."}),(0,n.jsx)("span",{className:" text-red-600  text-2xl",children:"."}),(0,n.jsx)("span",{className:" text-red-600  text-2xl",children:"."})]})},622:function(e,t,r){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=r(2265),s=Symbol.for("react.element"),o=Symbol.for("react.fragment"),a=Object.prototype.hasOwnProperty,l=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function q(e,t,r){var n,o={},f=null,u=null;for(n in void 0!==r&&(f=""+r),void 0!==t.key&&(f=""+t.key),void 0!==t.ref&&(u=t.ref),t)a.call(t,n)&&!c.hasOwnProperty(n)&&(o[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===o[n]&&(o[n]=t[n]);return{$$typeof:s,type:e,key:f,ref:u,props:o,_owner:l.current}}t.Fragment=o,t.jsx=q,t.jsxs=q},7437:function(e,t,r){"use strict";e.exports=r(622)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=3026)}),_N_E=e.O()}]);
--- .next/static/chunks/app/(front)/newregional/(quotePopup)/quotePopupRegionalManagerProperties/page-c6ec3ae1367b3bf1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[393,751,837,395,912,952,571,783],{6316:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[724,971,864,744],function(){return n(n.s=6316)}),_N_E=n.O()}]);
