ccb55ba0f89ad2c6615fbba1f332e922978feaf0
swsrkparmar09 <swsrkparmar09@gmail.com>
2024-01-30 18:37:56 +0530

    image and packages wise vendor show

---
+++ public/images&icons/pentahouse25-01-2024/footer.blade.php
@@ -0,0 +1,235 @@
+<!-- Footer -->
+<footer class="footer-section">
+	<div class="container">
+		<div class="footer-cta pt-5 pb-5">
+			<div class="row">
+				<div class="col-xl-4 col-md-4 mb-30">
+					<div class="single-cta">
+						<i class="fas fa-map-marker-alt"></i>
+						<div class="cta-text">
+							<h4>Sales Office</h4>
+							<span>
+								<a target="_blank" href="https://maps.app.goo.gl/2oKfUVZzUgXAky168">
+									32 Ang Mo Kio Industrial Park 2 <br>
+									#06-15/16 Sing Industrial Complex <br>
+									Singapore 569510</a>
+							</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-4 col-md-4 mb-30">
+					<div class="single-cta">
+						<i class="fas fa-phone"></i>
+						<div class="cta-text">
+							<h4>Call us</h4>
+							<span><a target="_blank" href="tel:+6564849000">Tel: +65 6484 9000</a> </span><br>
+							<span><a target="_blank" href="tel:+6564846636">Tel: +65 6484 6636</a> </span><br>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-4 col-md-4 mb-30">
+					<div class="single-cta">
+						<i class="far fa-envelope-open"></i>
+						<div class="cta-text">
+							<h4>Mail us</h4>
+							<span><a target="_blank" href="mailto:info@penta.com.sg">info@penta.com.sg</a></span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-content pt-5 pb-5">
+			<div class="row">
+				<div class="col-xl-4 col-lg-4 mb-50">
+					<div class="footer-widget">
+						<div class="footer-logo">
+							<a href="{{route('site.home')}}"><img src="{{asset('img/new_Penta_Logo_w.png')}}" class="img-fluid" alt="logo"></a>
+						</div>
+						<div class="footer-text">
+							<p class="FootlogoContent">
+								We design our products carefully. From the hands of our designers right into yours...</p>
+						</div>
+						<div class="footer-social-icon">
+							<span>Follow us</span>
+							<a target="_blank" href="https://www.facebook.com/pentalighting"><i class="fab fa-facebook-f facebook-bg"></i></a>
+							<a target="_blank" href="https://www.linkedin.com/company/penta-lighting/about/"><i
+										class="fab fa-linkedin linkedin-bg"></i></a>
+							<a target="_blank" href="https://www.instagram.com/pentacomsg/"><i class="fab fa-instagram instagram-bg"></i></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-2 col-lg-4 col-md-6 mb-30">
+					<div class="footer-widget">
+						<div class="footer-widget-heading">
+							<h3>Useful Links</h3>
+						</div>
+						<ul>
+							<li><a href="{{route('site.careers')}}">Careers</a></li>
+							<li><a href="{{route('site.contactus')}}">Contact Us</a></li>
+							<li><a href="#">Terms & Privacy</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-4 col-md-6 mb-30">
+					<div class="footer-widget timings">
+						<div class="footer-widget-heading">
+							<h3>Opening Hours</h3>
+						</div>
+						<p>Mon - Fri 8.30am - 5.30pm</p>
+						<p>Saturday - Closed</p>
+						<p>Sunday - Closed</p>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-4 col-md-6 mb-50">
+					<div class="footer-widget">
+						<div class="footer-widget-heading">
+							<h3>Subscribe</h3>
+						</div>
+						<div class="footer-text mb-25">
+							<p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+						</div>
+						<div class="subscribe-form">
+							<form action="#">
+								<input type="email" placeholder="Email Address">
+								<button><i class="fab fa-telegram-plane"></i></button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copyright-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-6 col-lg-6 ">
+					<div class="copyright-text">
+						<p>Copyright © 2023-24 Penta Lighting Pte Ltd , All Rights Reserved</p>
+					</div>
+				</div>
+				<div class="col-xl-6 col-lg-6 text-right">
+					<div class="footer-menu">
+						<ul style="text-align: right;">
+							<li><a href="{{route('site.contactus')}}">Contact Us</a></li>
+							<li><a href="{{route('site.careers')}}">Careers</a></li>
+							<li><a href="#">Terms & Privacy</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- ///////////////////////////////// -->
+<!-- ///////////////////////////// -->
+<!-- //////////////////////// -->
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  -->
+
+<!-- Include jQuery (optional but recommended) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.js"></script>
+
+<!-- Initialize the Slick Slider in a script tag -->
+<script>
+    $(document).ready(function () {
+        $('.slick-slider').slick({
+            loop: true,
+            arrows: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [
+                {
+                    breakpoint: 1140,
+                    settings: {
+                        arrows: false,
+
+                        slidesToShow: 3,
+                        autoplay: false
+                    }
+                },
+                {
+                    breakpoint: 991,
+                    settings: {
+                        arrows: false,
+
+                        slidesToShow: 2,
+                        autoplay: false
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        arrows: false,
+
+                        slidesToShow: 2,
+                        autoplay: false
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+
+                        autoplay: false,
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+
+
+        $('.slick-sliderTwo').slick({
+            loop: true,
+            arrows: true,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [
+                {
+                    breakpoint: 991,
+                    settings: {
+                        arrows: false,
+                        autoplay: false,
+
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        arrows: false,
+                        autoplay: false,
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        autoplay: false,
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+
+        $(".{{$active}}").addClass('active');
+    });
+</script>
