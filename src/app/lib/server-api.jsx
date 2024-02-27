e314bbf1c015615d8a85b452b3d6dc50f627f37b
meravisws@13022019 <ravisws123@gmail.com>
2024-02-27 13:18:26 +0530

    get magazine error serch page

--- src/app/lib/server-api.jsx
+++ src/app/lib/server-api.jsx
@@ -1,16 +1,25 @@
 export async function getCategories() {
     const res = await fetch(`${process.env.BASE_API_URL}category`, { cache: 'force-cache' })
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const vendorRes = await res.json()
     return vendorRes
 }
 export async function getMagazines() {
     const res = await fetch(`${process.env.BASE_API_URL}magazine`, { cache: 'no-cache' })
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const vendorRes = await res.json()
     return vendorRes
 }
 
 export async function getBlogs() {
     const res = await fetch(`${process.env.BASE_API_URL}blog-home?limit=3&offset=0`, { cache: 'force-cache' })
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const blogRes = await res.json()
     return blogRes
 }
@@ -45,29 +54,44 @@ export async function getVendors(props) {
 
 export  async function getPages(slug) {
     const res = await fetch(`${process.env.BASE_API_URL}page/${slug}`, { cache: 'no-cache' })
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const pageRes = await res.json()
     return pageRes
 }
 export async function getPostMeta() {
     const res = await fetch(`${process.env.BASE_API_URL}post-meta`, { cache: 'force-cache' })
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const pageMetaRes = await res.json()
     return pageMetaRes
 }
 
 export async function getSiteSetting() {
     const res = await fetch(`${process.env.BASE_API_URL}site_setting`, { cache: 'force-cache' })
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const siteSettingRes = await res.json()
     return siteSettingRes
 }
 
 export  async function getMagazineData(slug) {
     const res = await fetch(`${process.env.BASE_API_URL}magazine/${slug}`, { cache: 'force-cache' })
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const magazineDataRes = await res.json()
     return magazineDataRes
 }
 
 export  async function getMagazineAllData() {
     const res = await fetch(`${process.env.BASE_API_URL}magazine`, { cache: 'force-cache' })
+    if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
     const magazineAllDataRes = await res.json()
     return magazineAllDataRes
 }
